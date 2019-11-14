69106695d7ed45255d9b6d34dd7b1cc782901cfe
Louis Taylor <louis@kragniz.eu>
2019-11-14 15:14:27 +0000

    Add superblock struct
    Signed-off-by: Louis Taylor <louis@kragniz.eu>

--- amnesiafs.h
+++ amnesiafs.h
@@ -1,6 +1,18 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 #ifndef AMNESIAFS_H
 #define AMNESIAFS_H
 
 #define AMNESIAFS_MAGIC 0xdec0ded;
+
+struct amnesiafs_super_block {
+	uint64_t magic;
+	uint64_t version;
+
+	char padding[4073];
+};
+
+_Static_assert(sizeof(struct amnesiafs_super_block) == 4096,
+	       "amnesiafs_super_block must be a constent size");
 
 #endif
