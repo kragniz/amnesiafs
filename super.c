a69072e4129f93552c5a88d3a11218b0c3ee3630
Louis Taylor <louis@kragniz.eu>
2019-11-25 23:22:11 +0000

    Start doing some actual file things
    Signed-off-by: Louis Taylor <louis@kragniz.eu>

--- super.c
+++ super.c
@@ -6,6 +6,7 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/slab.h>
+#include <linux/stat.h>
 
 #include "amnesiafs.h"
 #include "config.h"
@@ -20,6 +21,72 @@ static void amnesiafs_put_super(struct super_block *sb)
 static struct super_operations const amnesiafs_super_ops = {
 	.put_super = amnesiafs_put_super,
 	.statfs = simple_statfs,
+};
+
+struct amnesiafs_inode {
+	mode_t mode;
+	uint64_t inode_no;
+	uint64_t data_block_number;
+
+	union {
+		uint64_t file_size;
+		uint64_t dir_children_count;
+	};
+};
+
+struct amnesiafs_dir_record {
+	char filename[255];
+	uint64_t inode_no;
+};
+
+static int amnesiafs_iterate(struct file *filp, struct dir_context *ctx)
+{
+	loff_t pos;
+	struct inode *inode;
+	struct super_block *sb;
+	struct buffer_head *bh;
+	struct amnesiafs_inode *sfs_inode;
+	struct amnesiafs_dir_record *record;
+	int i;
+
+	pos = ctx->pos;
+	inode = filp->f_path.dentry->d_inode;
+	sb = inode->i_sb;
+
+	if (pos) {
+		return 0;
+	}
+
+	sfs_inode = inode->i_private;
+
+	if (!S_ISDIR(sfs_inode->mode)) {
+		amnesiafs_err(
+			"inode [%llu][%lu] for fs object [%s] not a directory",
+			sfs_inode->inode_no, inode->i_ino,
+			filp->f_path.dentry->d_name.name);
+		return -ENOTDIR;
+	}
+
+	bh = sb_bread(sb, sfs_inode->data_block_number);
+	BUG_ON(!bh);
+
+	record = (struct amnesiafs_dir_record *)bh->b_data;
+	for (i = 0; i < sfs_inode->dir_children_count; i++) {
+		dir_emit(ctx, record->filename, 255, record->inode_no,
+			 DT_UNKNOWN);
+		ctx->pos += sizeof(struct amnesiafs_dir_record);
+
+		pos += sizeof(struct amnesiafs_dir_record);
+		record++;
+	}
+	brelse(bh);
+
+	return 0;
+}
+
+const struct file_operations amnesiafs_dir_operations = {
+	.owner = THIS_MODULE,
+	.iterate = amnesiafs_iterate,
 };
 
 static int amnesiafs_fill_super(struct super_block *sb, void *data, int silent)
@@ -75,6 +142,7 @@ static int amnesiafs_fill_super(struct super_block *sb, void *data, int silent)
 
 	root->i_ino = 0;
 	root->i_sb = sb;
+	root->i_fop = &amnesiafs_dir_operations;
 	root->i_atime = root->i_mtime = root->i_ctime = current_time(root);
 	inode_init_owner(root, NULL, S_IFDIR);
 
