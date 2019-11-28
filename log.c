0c27ba3c0aee2ca38efb73e2864a0f873774cf96
Louis Taylor <louis@kragniz.eu>
2019-11-28 15:50:53 +0000

    Show line number in amnesiafs_debug
    Signed-off-by: Louis Taylor <louis@kragniz.eu>

--- log.c
+++ log.c
@@ -13,3 +13,17 @@ void amnesiafs_msg(const char *level, const char *fmt, ...)
 	printk("%samnesiafs: %pV\n", level, &vaf);
 	va_end(args);
 }
+
+void amnesiafs_msg_line_number(const char *level, const char *fname, int lineno,
+			       const char *fxname, const char *fmt, ...)
+{
+	struct va_format vaf;
+	va_list args;
+
+	va_start(args, fmt);
+	vaf.fmt = fmt;
+	vaf.va = &args;
+	printk("%samnesiafs: %s:%d (%s) %pV\n", level, fname, lineno, fxname,
+	       &vaf);
+	va_end(args);
+}
--- log.h
+++ log.h
@@ -6,12 +6,18 @@
 void amnesiafs_msg(const char *level, const char *fmt, ...)
 	__attribute__((format(printf, 2, 3)));
 
+void amnesiafs_msg_line_number(const char *level, const char *fname, int lineno,
+			       const char *fxname, const char *fmt, ...)
+	__attribute__((format(printf, 5, 6)));
+
 #define amnesiafs_err(fmt, ...) amnesiafs_msg(KERN_ERR, fmt, ##__VA_ARGS__)
 
 #define amnesiafs_info(fmt, ...) amnesiafs_msg(KERN_INFO, fmt, ##__VA_ARGS__)
 
 #if defined(DEBUG)
-#define amnesiafs_debug(fmt, ...) amnesiafs_msg(KERN_DEBUG, fmt, ##__VA_ARGS__)
+#define amnesiafs_debug(fmt, ...)                                              \
+	amnesiafs_msg_line_number(KERN_DEBUG, __FILE__, __LINE__, __func__,    \
+				  fmt, ##__VA_ARGS__)
 #else
 #define amnesiafs_debug(fmt, ...) no_printk(KERN_DEBUG fmt, ##__VA_ARGS__)
 #endif
